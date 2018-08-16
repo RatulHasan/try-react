d383dc15ff390f4c481f406ae15128ad4497be35
Ratul Hasan <ratuljh@gmail.com>
2018-08-16 12:23:25 +0600

    React YouTube

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PostList from './posts/PostList'
+import ReactYoutubeExample from './thirdParty/ReactYoutubeExample'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+            <ReactYoutubeExample videoId='s4aIk0x5Eus'/>
         </header>
           <PostList/>
       </div>
---
+++ src/thirdParty/ReactYoutubeExample.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import YouTube from 'react-youtube';
+// https://www.youtube.com/watch?v=s4aIk0x5Eus
+// https://youtu.be/s4aIk0x5Eus
+
+class ReactYoutubeExample extends Component {
+
+    videoOnReady(event) {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+    }
+
+    render() {
+        const opts = {
+            height: '390',
+            width: '640',
+            playerVars: { // https://developers.google.com/youtube/player_parameters
+                autoplay: 1
+            }
+        };
+
+        const {videoId} = this.props
+
+        return (
+            <YouTube
+                videoId={videoId}
+                opts={opts}
+                onReady={this.videoOnReady}
+            />
+        );
+    }
+}
+
+export default ReactYoutubeExample;
