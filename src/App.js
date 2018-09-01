d5aae2c6711792a84b317e439cbe15303cbe7d13
ratul <ratuljh@gmail.com>
2018-09-02 02:50:50 +0600

    React Markdown Example

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        {/*<header className="App-header">*/}
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          {/*<h1 className="App-title">Welcome to React</h1>*/}
             {/*<ReactYoutubeExample videoId='s4aIk0x5Eus'/>*/}
-        </header>
+        {/*</header>*/}
           <PostList/>
       </div>
     );
